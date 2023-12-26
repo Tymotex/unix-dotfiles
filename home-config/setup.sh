64f6a9bb0833df8ebdb9681b3df1841e212f6f07
Tim Zhang <admin@timz.dev>
2023-12-26 15:30:05 +1100

    Add simple zshrc with oh-my-zsh and powerlevel10k.

---
+++ home-config/setup.sh
@@ -0,0 +1,8 @@
+# Assumes that Zsh is already installed.
+
+# Set up oh-my-zsh.
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Set up powerlevel10k.
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+# Assumes that Zsh is already installed.
+
+# Set up oh-my-zsh.
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Set up powerlevel10k.
+
